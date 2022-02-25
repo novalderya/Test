3ce913b78c7f6bc4dc1c4913d19b5261ef4c5a18
yannpoupon <y.poupon@technologyandstrategy.com>
2022-02-25 10:54:04 +0100

    Finir Exercice + correction
    Réalisation des méthodes que je savais réaliser et correction dans une copie de mon fichier afin de garder une trace de mon travail.

--- Tp dictionnaire ordonne/DictionnaireOrdonne_version_corrige.py
+++ Tp dictionnaire ordonne/DictionnaireOrdonne_version_corrige.py
@@ -8,15 +8,21 @@ from typing import Dict
 class DictionnaireOrdonne() :
     """Classe permettant de réaliser un dictionnaire que l'on peut trier avec une valeur
     """
-
-    def __init__(self,dictionnaire={}):
+    #Ajout de la possibilite de creer avec en entree des valeurs ou un autre dictionnaire
+    def __init__(self,base={},**dictionnaire):
         self.cles=[]
         self.valeur=[]
-
+        for i,elt in dictionnaire.items() :
+            self.cles.append(i)
+            self.valeur.append(elt)
+        if type(base) in (dict, DictionnaireOrdonne) :
+            for cle in base :
+                #Methode plus rapide pour assigner les valeurs 
+                self[cle]=base[cle]
 
 
     def __setitem__(self,cle,valeur):
-        """Fonction permettant de definir une variable du dictionnaire par dico[cle]=valeur"""
+        """Fonction permettant de définir une variable du dictionnaire par dico[cle]=valeur"""
         if cle in self.cles :
             index=self.cles.index(cle)
             self.valeur[index]=valeur
@@ -30,7 +36,10 @@ class DictionnaireOrdonne() :
         if cle in self.cles :
             index=self.cles.index(cle)
             return self.valeur[index]
-        return None
+        #rajout d'une exception pour traiter le cas ou la cle donner n'existe pas 
+        else : 
+            raise KeyError("La clé demandé n'existe pas dans ce dictionnaire")
+
 
     def __delitem__(self,cle):
         """Fonction permettant de detruire une cle donne"""
@@ -38,6 +47,9 @@ class DictionnaireOrdonne() :
             index=self.cles.index(cle)
             del self.cles[index]
             del self.valeur[index]
+             #rajout d'une exception pour traiter le cas ou la cle donner n'existe pas 
+        else : 
+            raise KeyError("La clé demande n'existe pas dans ce dictionnaire")
 
     def __contains__(self, cle):
         """Fonction permettant de savoir si la cle est presente dans la liste de cle defini"""
@@ -46,6 +58,7 @@ class DictionnaireOrdonne() :
         
 
     def __len__(self) :
+        """Fonction renvoyant la longueur du dictionnaire"""
         return len(self.cles)
 
     def __repr__(self) :
@@ -56,6 +69,14 @@ class DictionnaireOrdonne() :
         chaine+="'{}': {}".format(self.cles[-1],self.valeur[-1])
         chaine+="}"
         return chaine
+
+    #Ajout de str que je n'avais pas défini 
+    def __str__(self):
+        """Fonction appelée quand on souhaite afficher le dictionnaire grâce
+        à la fonction 'print' ou le convertir en chaîne grâce au constructeur
+        'str'. On redirige sur __repr__"""
+        
+        return repr(self)
     
     def sort(self):
         """Fonction permettant de trier le dictionnaire par rapport a la liste des valeurs"""
@@ -66,7 +87,7 @@ class DictionnaireOrdonne() :
             valeur.append(self.valeur[index])
         self.cles=liste_cle_triee
         self.valeur=valeur
-
+    #Ma version 
     def reverse(self):
         """Fonction permettant d'inverser le dictionnaire"""
         cles=[]
@@ -78,6 +99,20 @@ class DictionnaireOrdonne() :
         self.cles=cles
         self.valeur=valeurs
 
+    # #version propose par la correction 
+    # def reverse(self):
+    #     """Inversion du dictionnaire"""
+    #     # On crée deux listes vides qui contiendront le nouvel ordre des clés
+    #     # et valeurs
+    #     cles = []
+    #     valeurs = []
+    #     for cle, valeur in self.items():
+    #         # On ajoute les clés et valeurs au début de la liste
+    #         cles.insert(0, cle)
+    #         valeurs.insert(0, valeur)
+    #     # On met ensuite à jour nos listes
+    #     self._cles = cles
+    #     self._valeurs = valeurs
 
     def __iter__(self) : 
         """Fonction permettant l'acces lorsque on utilise for i in dictionnaire"""
@@ -91,30 +126,47 @@ class DictionnaireOrdonne() :
         """Fonction retournant les valeurs du dictionnaire"""
         return(self.valeur)
 
+    #Ma version
     def items(self): 
         """Fonction retournant une liste de tuple contenant les valeurs et les cles"""
         list=[]
         for i in range(len(self.cles)):
             list.append((self.cles[i],self.valeur[i]))
         return(list)
+    # #Autre version possible , correction 
+    # def items(self):
+    #     """Renvoie un générateur contenant les couples (cle, valeur)"""
+    #     for i, cle in enumerate(self._cles):
+    #         valeur = self._valeurs[i]
+    #         yield (cle, valeur)
+    
 
     def __add__(self,dictionnaire):
         """Fonction permettant d'ajouter deux dictionnaires entre eux"""
-        nouveau=DictionnaireOrdonne()
-        for cle in self.cles :
-            nouveau.cles.append(cle)
-        for valeur in self.valeur :
-            nouveau.valeur.append(valeur)
+        #Rajout d'une verification de type avant l'addition et on indique le problème rencontré
+        if type(dictionnaire) is not type(self):
+            raise TypeError( \
+                "Impossible de concaténer {0} et {1}".format( \
+                type(self), type(dictionnaire)))
+        else:
+            nouveau=DictionnaireOrdonne()
+            # for cle in self.cles :
+            #     nouveau.cles.append(cle)
+            # for valeur in self.valeur :
+            #     nouveau.valeur.append(valeur)
+            
+            #Moins de ligne de code pour la même opérations
+            for cle in self.cles :
+                nouveau[cle]=self[cle]
 
-        for i in range (len(dictionnaire)) :
-            #On regarde si la cle du deuxieme dictionnaire n'est pas dans le premier
-            if dictionnaire.cles[i] not in self.cles :
-                nouveau.cles.append(dictionnaire.cles[i])
-                nouveau.valeur.append(dictionnaire.valeur[i])
-            #Si la cle est présente on va addittionner la valeur des deux dictionnaires
-            else :
-                nouveau.valeur[i]+=dictionnaire.valeur[i]
-
+            for i in range (len(dictionnaire)) :
+                #On regarde si la cle du deuxieme dictionnaire n'est pas dans le premier
+                if dictionnaire.cles[i] not in self.cles :
+                    nouveau.cles.append(dictionnaire.cles[i])
+                    nouveau.valeur.append(dictionnaire.valeur[i])
+                #Si la cle est présente on va addittionner la valeur des deux dictionnaires
+                else :
+                    nouveau.valeur[i]+=dictionnaire.valeur[i]
         return nouveau
 
     
@@ -149,6 +201,12 @@ if __name__=="__main__" :
     print(fruits)
     fruits.reverse()
     print(fruits)
+
+    test_creation_liste= DictionnaireOrdonne(carrote = 26, haricot = 23)
+    print(test_creation_liste)
+    test_creation_dictionnaire= DictionnaireOrdonne(test_creation_liste)
+    print(test_creation_dictionnaire)
+
 
 
 
